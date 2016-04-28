16ef9ddfb6c2da5348d07910a78973e1e37a2ed6
SKitajima <a3ynxo@gmail.com>
2016-04-28 16:29:32 +0900

    うー

--- explosionPOS/cms/urls.py
+++ explosionPOS/cms/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from cms import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^shop/$', views.shop, name='shop'),
     url(r'^buy/$', views.buy, name='buy'),
 ]
--- explosionPOS/cms/views.py
+++ explosionPOS/cms/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 from cms.models import User, Item, Sale
 
 # Create your views here.
-def buy(request):
+def shop(request):
     sale = Sale.objects.all().order_by('id')
-    return render(request,'cms/buy.html')
+    items = Item.objects.all().order_by('code')
+    users = User.objects.all().order_by('student_id')
+    context = {'items': items, 'users':users}
+    return render(request,'cms/shop.html',context)
+
+def buy(request):
+    selected_user = get_object_or_404(User,pk=request.POST['user'])
+    selected_item = get_object_or_404(Item,pk=request.POST['item'])
+    selected_value = request.POST['value']
+    selected_price = 
+    
